98cf7c4c47fa3da09cd3de1abb039255d25c934a
leewol <lsee5265@gmail.com>
2023-02-25 01:11:11 +0900

    [Feat] 뒤로가기, 첫 페이지로

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,11 @@
+import { Link, useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowCircleLeft,
+  faHouse,
+  faSun,
+  faMoon,
+} from "@fortawesome/free-solid-svg-icons";
 import styled from "styled-components";
 
 interface IHeaderProps {
@@ -6,21 +14,56 @@ interface IHeaderProps {
 
 const HeaderContainer = styled.header`
   height: 10vh;
-  display: flex;
-  justify-content: center;
+  display: grid;
+  grid-template-columns: 1fr 3fr 1fr;
   align-items: center;
-  margin: 20px 0;
+  margin: 50px 0;
+  .header-icon {
+    font-size: 28px;
+    &:hover {
+      cursor: pointer;
+      opacity: 0.8;
+    }
+  }
 `;
 
 const Title = styled.h1`
   color: ${(props) => props.theme.accentColor};
   font-size: 48px;
+  text-align: center;
+`;
+
+const LeftBox = styled.div``;
+
+const RightBox = styled.div`
+  text-align: right;
+  .header-icon:first-child {
+    margin-right: 10px;
+  }
 `;
 
 function Header({ title }: IHeaderProps) {
+  const navigate = useNavigate();
+  const onBackClick = () => {
+    navigate(-1);
+  };
+
   return (
     <HeaderContainer>
+      <LeftBox>
+        <FontAwesomeIcon
+          onClick={onBackClick}
+          className="header-icon"
+          icon={faArrowCircleLeft}
+        />
+      </LeftBox>
       <Title>{title}</Title>
+      <RightBox>
+        <Link to="/">
+          <FontAwesomeIcon className="header-icon" icon={faHouse} />
+        </Link>
+        <FontAwesomeIcon className="header-icon" icon={faSun} />
+      </RightBox>
     </HeaderContainer>
   );
 }
